16a1cb6c846be3f1d34056207f35171ae76bb166
Michal Marhoul <marhoul.m@seznam.cz>
2025-05-07 16:54:14 +0200

    TestProject

---
+++ TestProject/Program.cs
@@ -0,0 +1,9 @@
+﻿string pangram = "The quick brown fox jumps over the lazy dog";
+string [] pangramSplit = pangram.Split(" ");
+string finalSentence = "";
+
+foreach (var word in pangramSplit){
+   string result = new string (word.Reverse().ToArray());
+   finalSentence += result + " ";
+}
+Console.WriteLine(finalSentence);
