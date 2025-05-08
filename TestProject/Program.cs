48e3f0bdd6e2b7a67f6da3e10f725aadf366ca25
Michal Marhoul <marhoul.m@seznam.cz>
2025-05-08 11:28:45 +0200

    Verze 2.0

--- TestProject/Program.cs
+++ TestProject/Program.cs
@@ -1,9 +1,19 @@
-﻿string pangram = "The quick brown fox jumps over the lazy dog";
-string [] pangramSplit = pangram.Split(" ");
-string finalSentence = "";
+﻿const string input = "<div><h2>Widgets &trade;</h2><span>5000</span></div>";
 
-foreach (var word in pangramSplit){
-   string result = new string (word.Reverse().ToArray());
-   finalSentence += result + " ";
-}
-Console.WriteLine(finalSentence);
+string quantity = "";
+string output = "";
+
+int firstSpanPos = input.IndexOf("<span>") + 6;
+int lastSpanPos = input.IndexOf("</span>");
+int quatityLenght = lastSpanPos - firstSpanPos;
+quantity = input.Substring(firstSpanPos, quatityLenght);
+
+const string tradeSymbol = "&trade;";
+const string regSymbol = "&reg;";
+output = input.Replace(tradeSymbol, regSymbol);
+int firstDivPos = output.IndexOf("<div>") + 5;
+int lastDivPos = output.IndexOf("</div>");
+int outputLenght = lastDivPos - firstDivPos;
+output = output.Substring(firstDivPos, outputLenght);
+Console.WriteLine($"Quantity: {quantity}");
+Console.WriteLine($"Output: {output}");
