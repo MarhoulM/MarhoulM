499044cbfead30705f6241905e9f9464fe249bfe
Michal Marhoul <marhoul.m@seznam.cz>
2025-05-10 13:57:32 +0200

    Add new method

--- CV - C# console/program.cs
+++ CV - C# console/program.cs
@@ -36,16 +36,14 @@ do
                     Console.WriteLine("\x1b[4m1. Kontaktní a osobní údaje.\x1b[0m");
                     Console.WriteLine(@"Telefon: +420 723 028 376 | E-mail: marhoul.m@seznam.cz | LinkedIn: www.linkedin.com/in/michal-marhoul-223440132
 Višňová, Česká republika");
-                    Console.WriteLine("Stiskni klávesu Enter pro pokračování.");
-                    readResult = Console.ReadLine();
+                    Confirmation();
                     break;
                 }
             case "2":
                 {
                     Console.WriteLine("\x1b[4m2. Profil.\x1b[0m");
                     Console.WriteLine("Při zprovozňování zařízení v zahraničí se jako PLC programátor často musím spolehnout hlavně na sebe a svůj úsudek. Když k tomu přidám odhodlání dotáhnout věci do konce, dostanete přesně mě.");
-                    Console.WriteLine("Stiskni klávesu Enter pro pokračování.");
-                    readResult = Console.ReadLine();
+                    Confirmation();
                     break;
                 }
             case "3":
@@ -55,8 +53,7 @@ Višňová, Česká republika");
                     Console.WriteLine("TECHNOLOG – SAINT-GOBAIN SEKURIT ČR SPOL. S R. O. / HOŘOVICE\nBřezen 2017 – Únor 2018\n- Vytváření technologických postupů\n- Vytváření kontrolních plánů a návodů\n- Dohled nad svěřenou linkou\n- Zavádění nových výrobků na linku\n- Seřizování linky\n- Spolupráce s oddělením kvality a pracovníky na lince\n- Normování\n");
                     Console.WriteLine("TECHNOLOG – GEOMINE A.S. / PŘÍBRAM\nÚnor 2015 – Únor 2017\n- Uvádění nových výrobku do výroby\n- Vytváření výkresové dokumentace\n- Vytváření rozvinu nových dílů\n- Dohled nad ověřovací sérií nových dílů\n- Vytváření technologických postupů\n- Spolupráce s oddělením kontroly a obchodu\n- Normování\n- Navrhování přípravků\n- Komunikace se zákazníkem\n");
                     Console.WriteLine("CNC PROGRAMÁTOR – GEOMINE A.S. / PŘÍBRAM\nSrpen 2014 – Únor 2015\n- Programování CNC Ohraňovacího lisu\n- Příprava nářadí\n- Výroba plechových dílů (ohraňovací lis)\n- Kontrola a měření vyrobených dílů\n");
-                    Console.WriteLine("Stiskni klávesu Enter pro pokračování.");
-                    readResult = Console.ReadLine();
+                    Confirmation();
                     break;
                 }
             case "4":
@@ -68,8 +65,7 @@ Višňová, Česká republika");
                     Česká zemědělská univerzita v Praze, 2017 – 2020 ");
                     Console.WriteLine(@"Střední průmyslová škola a vyšší odborná škola, Příbram
                     Obor: Strojírenství počítačové, 2010 – 2014");
-                    Console.WriteLine("Stiskni klávesu Enter pro pokračování.");
-                    readResult = Console.ReadLine();
+                    Confirmation();
                     break;
                 }
             case "5":
@@ -78,8 +74,7 @@ Višňová, Česká republika");
                     Console.WriteLine("Technické dovednosti:\n- Programovací jazyky: STL\n- konfigurace sítí a síťových prvků\n- Nástroje: Siemens TIA Portal (pokročilý), SIMATIC Step7 (základy), Microsoft Office (pokročilý), AutoCAD (mírně pokročilý), Autodesk Inventor (mírně pokročilý)\n\nMěkké dovednosti:\n- Dohled nad projekty\n- Týmová práce\n");
                     Console.WriteLine("Jazyky:\n Angličtina: B2 (středně pokročilá)\nNěmčina: A2\nČeština: rodilý mluvčí\n");
                     Console.WriteLine("Zájmy:\n- Turistika\n- Volejbal\n- 3D tisk\n- studium cizích jazyků");
-                    Console.WriteLine("Stiskni klávesu Enter pro pokračování.");
-                    readResult = Console.ReadLine();
+                    Confirmation();
                     break;
                 }
             case "6":
@@ -106,8 +101,7 @@ Višňová, Česká republika");
                     Console.WriteLine("Technické dovednosti:\n- Programovací jazyky: STL\n- konfigurace sítí a síťových prvků\n- Nástroje: Siemens TIA Portal (pokročilý), SIMATIC Step7 (základy), Microsoft Office (pokročilý), AutoCAD (mírně pokročilý), Autodesk Inventor (mírně pokročilý)\n\nMěkké dovednosti:\n- Dohled nad projekty\n- Týmová práce\n");
                     Console.WriteLine("Jazyky:\nAngličtina: B2 (středně pokročilá)\nNěmčina: A2\nČeština: rodilý mluvčí\n");
                     Console.WriteLine("Zájmy:\n- Turistika\n- Volejbal\n- 3D tisk\n- studium cizích jazyků");
-                    Console.WriteLine("Stiskni klávesu Enter pro pokračování.");
-                    readResult = Console.ReadLine();
+                    Confirmation();
                     break;
                 }
             case "n":
@@ -138,8 +132,7 @@ Višňová, Česká republika");
                             default:
                                 {
                                     Console.WriteLine("Zadán prázdný nebo chybný znak!");
-                                    Console.WriteLine("Stiskni klávesu Enter pro pokračování.");
-                                    readResult = Console.ReadLine();
+                                    Confirmation();
                                     break;
                                 }
                         }
@@ -161,10 +154,15 @@ Višňová, Česká republika");
             default:
                 {
                     Console.WriteLine("Zadán prázdný nebo chybný znak!");
-                    Console.WriteLine("Stiskni klávesu Enter pro pokračování.");
-                    readResult = Console.ReadLine();
+                    Confirmation();
                     break;
                 }
         }
     }
 } while (option != "exit");
+
+void Confirmation()
+{
+    Console.WriteLine("Stiskni klávesu Enter pro pokračování.");
+    readResult = Console.ReadLine();
+}
